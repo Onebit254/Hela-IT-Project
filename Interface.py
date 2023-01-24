7714ddfdd17751ea503d5be4092bc28008ec0adb
Onebit254 <68463244+Onebit254@users.noreply.github.com>
2023-01-24 14:00:49 +0300

    Update1
    Update

--- Interface.py
+++ Interface.py
@@ -1,4 +1,16 @@
 from tkinter import *
+from PIL import ImageTk
 class Login:
     def __init__(self, root):
-        pass
+        self.root = root
+        self.root.title("Login System")
+        self.root.geometry("1099x600+100+50")
+
+        #Background Image
+        self.bg=ImageTk.PhotoImage(file="images/58.jpg")
+        self.bg_image=Label(self.root, image=self.bg).place(x=0, y=0, relwidth=1, relheight=1)
+
+
+root = Tk()
+obj = Login(root)
+root.mainloop()
