9986cf3414519bf6b64789fd5d0fdec68412a2c2
Onebit254 <68463244+Onebit254@users.noreply.github.com>
2023-01-24 15:10:58 +0300

    update

--- Interface.py
+++ Interface.py
@@ -1,14 +1,20 @@
 from tkinter import *
-from PIL import ImageTk
+
 class Login:
     def __init__(self, root):
         self.root = root
         self.root.title("Login System")
         self.root.geometry("1099x600+100+50")
+        self.root.resizable(False, False)
 
         #Background Image
-        self.bg=ImageTk.PhotoImage(file="images/58.jpg")
-        self.bg_image=Label(self.root, image=self.bg).place(x=0, y=0, relwidth=1, relheight=1)
+        self.bg=PhotoImage(file="images/6.png")
+        self.bg_image=Label(self.root, image=self.bg).place(x=10, y=0, relwidth=1, relheight=1)
+
+        #Login Frame
+        Frame_login = Frame(self.root, bg="white")
+        Frame_login.place(x=250, y=100, width=600, height=400)
+
 
 
 root = Tk()
