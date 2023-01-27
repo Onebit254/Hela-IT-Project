d8ccccbf2687cc43947c77b1bf82e49149b170b3
Onebit_ke <68463244+Onebit254@users.noreply.github.com>
2023-01-27 04:00:49 +0300

    7

--- Interface.py
+++ Interface.py
@@ -29,16 +29,10 @@ class Login:
         self.password = Entry(Frame_login, font=("Goudy old style", 15), bg="#E7E6E6")
         self.password.place(x=30, y=250, width=320, height=35)
 
-        #CheckButton
-        self.checkvalue = IntVar
-        self.checkbtn = Checkbutton(Frame_login, text="Confirm", cursor="hand2", variable = self.checkvalue) 
-        self.checkbtn.place(x=30, y=300)
-
-
+        
         #SubmitButton
-        self.check_function = StringVar
-        forget = Button(Frame_login, text="forgot password?", bd=0, cursor="hand2", fg="black", bg="white").place(x=30, y=330), 
-        submit = Button(Frame_login,command=self.check_function, cursor="hand2", text="Submit", bg="black", fg="white").place(x=30, y=370)
+        forget = Button(Frame_login, text="forgot password?", bd=0, cursor="hand2", fg="black", bg="white").place(x=30, y=300), 
+        submit = Button(Frame_login,command=self.check_function, cursor="hand2", text="Submit", bg="black", fg="white").place(x=30, y=350)
 
     def check_function(self):
         if self.username.get() == "" or self.password.get() == "":
